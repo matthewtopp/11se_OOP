2890f836788a525acd7225e493f2ddb4b7f43022
m <matthew.topp@det.nsw.edu.au>
2026-05-06 23:34:15 +0000

    delete oop folder

--- oop/5a._fighter_class_activities.py
+++
@@ -1,8 +0,0 @@
-#Learning Intentions
-#1. Create a loop which simulates a fight and declares a winner
-#2. Test the game 
-#3. Implement the game with a private __health attribute
-
-import random
-
-class Fighter:
